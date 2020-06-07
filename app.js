395f26c0ce59533e2f10f268d71b4076a3e2d587
Utkarsh Sahu <utkarsh.sahu@alphafortress.com>
2020-06-07 23:17:58 +0530

    all corrections done

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const navBtn = document.getElementById("nav-btn");
-const navbar = document.getElementById("nav-bar");
+const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
 
 navBtn.addEventListener("click", () => {
